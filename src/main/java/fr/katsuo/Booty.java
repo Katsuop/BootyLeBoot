7bf7fd2825e81cd43191ab1ddedecbc820b2c1e4
Katsuop <luka.deruyck236@gmail.com>
2022-02-10 00:11:15 +0100

    initial project

--- src/main/java/fr/katsuo/Booty.java
+++ src/main/java/fr/katsuo/Booty.java
@@ -1,19 +1,18 @@
 package fr.katsuo;
 
+import fr.katsuo.event.Listener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
-import javax.security.auth.login.LoginException;
-
 public class Booty {
 
-    public static void main(String[] args) throws LoginException {
-        JDABuilder builder = JDABuilder.createDefault(args[0]);
-        builder.setActivity(Activity.watching("TV"));
-        builder.setToken()
+    public static void main(String[] args) throws Exception {
+        JDABuilder builder = JDABuilder.createDefault(Secret.TOKEN);
+        builder.addEventListeners(new Listener());
+        builder.setActivity(Activity.of(Activity.ActivityType.CUSTOM_STATUS, "tesr"));
         builder.build();
     }
 }
